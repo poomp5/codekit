83ea23bd3f0cee91f85995f3fb6041961a5812c0
poomp5 <poom27200@gmail.com>
2025-03-22 09:43:04 +0700

    navbar

---
+++ components/section-container.tsx
@@ -0,0 +1,7 @@
+export const SectionContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <section className="mx-auto max-w-[1110px] "></section>;
+};
---
+++ lib/fonts.ts
@@ -0,0 +1,6 @@
+import {IBM_Plex_Sans_Thai } from "next/font/google"
+
+export const fontIbmThai = IBM_Plex_Sans_Thai({
+    weight: ["400", "500", "600", "700", ],
+    subsets: ["thai"],
+})
